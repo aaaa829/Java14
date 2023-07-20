8cac76df3d115f1ee5294efa9a44e50a03b1a1ff
aaaa <iwaturu829@yahoo.co.jp>
2023-07-20 20:26:48 +0900

    Java14　OBJ

--- src/Java14.java
+++ src/Java14.java
@@ -2,8 +2,14 @@
 public class Java14 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		Hero h = new Hero();
+		h.name = "sasaki";
+		Hero h1 = new Hero();
+		h1.name = "sasaki";
+		System.out.println(h.equals(h1));
+		
+		Object o = new Hero();
+		
 	}
 
 }
